f3a444b52cd4eb920751e4a7876530cb1a54d1d7
jacobsanosh <jacobsanosh@gmail.com>
2022-12-02 11:58:12 +0530

    adding comonents

---
+++ src/components/Happy_customer/Happy_cust.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import styles from './Happy_cust.module.css'
+function Happy_cust() {
+  return (
+    <div className='styles'>Happy_cust</div>
+  )
+}
+
+export default Happy_cust
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,12 @@
-import { Footer, Navbar } from "../components";
+import { Faq, Footer, Happy_cust, Navbar } from "../components";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <Navbar />
+      <Faq/>
+      <Happy_cust/>
       <Footer />
     </div>
   );
