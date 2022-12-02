23b7e437fb75466fe8855c75b99704e81428091f
Alen4152 <alenchaks.aj@gmail.com>
2022-12-03 02:59:45 +0530

    updated

---
+++ src/data/foodData.js
@@ -0,0 +1,60 @@
+import one from "../assets/food/1.jpg";
+import two from "../assets/food/2.jpg";
+import three from "../assets/food/3.jpg";
+import four from "../assets/food/4.jpg";
+import five from "../assets/food/5.jpg";
+import six from "../assets/food/6.jpg";
+import seven from "../assets/food/7.jpg";
+const foodData = [
+  {
+    id: 1,
+    img: one,
+    title: "Kudakkachira House",
+    location: "Kannur",
+    food: "Fish Curry Meals",
+  },
+  {
+    id: 2,
+    img: two,
+    title: "Edakkatukudy house",
+    location: "Kothamangalam",
+    food: "Karimeen pollichath",
+  },
+  {
+    id: 3,
+    img: three,
+    title: "Valiyaveetil house",
+    location: "Kottayam",
+    food: "Chicken biriyani",
+  },
+  {
+    id: 4,
+    img: four,
+    title: "Karthalayam house",
+    location: "Pattikadu",
+    food: "Naadan oonu",
+  },
+  {
+    id: 5,
+    img: five,
+    title: "Maliyil House",
+    location: "Kochi",
+    food: "Fish fry meals",
+  },
+  {
+    id: 6,
+    img: six,
+    title: "Arakkal House",
+    location: "Kozhikode",
+    food: "Mutton biriyani",
+  },
+  {
+    id: 7,
+    img: seven,
+    title: "Kaduvakunnel House",
+    location: "Pala",
+    food: "Beef fry",
+  }
+
+];
+export default foodData;
