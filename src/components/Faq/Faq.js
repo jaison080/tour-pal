8400bd43773adc76b8f91f88c0f3ea1f8baf8ba3
jaison080 <jaisondennis080@gmail.com>
2022-12-02 14:26:30 +0530

    Updated

--- src/components/Faq/Faq.js
+++ src/components/Faq/Faq.js
@@ -1,36 +1,24 @@
-import React from 'react'
-import styles from './Faq.module.css'
+import React from "react";
+import faqData from "../../data/faqData";
+import styles from "./Faq.module.css";
 function Faq() {
   return (
     <div className={styles.faq_container}>
-        <h1 className={styles.heading}>FAQ</h1>
+      <div className={styles.heading}>FAQ</div>
       <div className={styles.contianer}>
-        <div className="sections">
-          <h2>Is there a speed limit?</h2>
-          <p>Revv allows up to 125 km/hr. However it is 80 km/hr in a few cities where some cars might be equipped with speed governors as per government directives. Revv strictly advises to follow local speed limits.</p>
-          <hr />
-        </div>
-        <div className="sections">
-          <h2>Can I extend/ cancel/ modify?</h2>
-          <p>Yes, extensions are possible subject to availability & charges. Cancellations & modifications will attract nominal charges as per our policy.
-          </p>
-          <hr />
-        </div>
-        <div className="sections">
-          <h2>Booking criteria & documents?</h2>
-          <p>Min. 21 years old, have valid original government ID (Aadhar, Passport, or PAN only) and a valid driving licence for “Light Motor Vehicles”, which is min. 1 year old at the time of starting the trip.
-          </p>
-          <hr />
-        </div>
-        <div className="sections">
-          <h2>Are there any restricted areas?</h2>
-          <p>Leh/Ladhakh, Spiti Valley & Kaza/Nako regions are not permitted to take Revv cars. Customer will be fully liable for any damages incurred to the car in that region.
-          </p>
-          
-        </div>
+        {faqData.map((item, index) => {
+          return (
+            <div key={index}>
+              <div className={styles.title}>{item.question}</div>
+              <br/>
+              <div className={styles.answer}>{item.answer}</div>
+              <hr />
+            </div>
+          );
+        })}
       </div>
     </div>
-  )
+  );
 }
 
-export default Faq
+export default Faq;
---
+++ src/data/faqData.js
@@ -0,0 +1,23 @@
+const faqData = [
+    {
+        id:1,
+        question:"Is there a speed limit?",
+        answer:"Revv allows up to 125 km/hr. However it is 80 km/hr in a few cities where some cars might be equipped with speed governors as per government directives. Revv strictly advises to follow local speed limits."
+    },
+    {
+        id:2,
+        question:"Can I extend/ cancel/ modify?",
+        answer:"Yes, extensions are possible subject to availability & charges. Cancellations & modifications will attract nominal charges as per our policy."
+    },
+    {
+        id:3,
+        question:"Booking criteria & documents?",
+        answer:"Min. 21 years old, have valid original government ID (Aadhar, Passport, or PAN only) and a valid driving licence for “Light Motor Vehicles”, which is min. 1 year old at the time of starting the trip."
+    },
+    {
+        id:4,
+        question:"Are there any restricted areas?",
+        answer:"Leh/Ladhakh, Spiti Valley & Kaza/Nako regions are not permitted to take Revv cars. Customer will be fully liable for any damages incurred to the car in that region."
+    }
+]
+export default faqData
