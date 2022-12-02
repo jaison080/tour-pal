ab394d82ab8232c0567afc55003cfa15944363b3
jacobsanosh <jacobsanosh@gmail.com>
2022-12-02 18:19:54 +0530

    demo

--- src/pages/peershare.js
+++ src/pages/peershare.js
@@ -1,6 +1,6 @@
 import React from "react";
 import styles from "../styles/peershare.module.css";
-function rentpage() {
+function peershare() {
   return (
     <div>
       <div className={styles.container}>
@@ -12,13 +12,28 @@ function rentpage() {
             id="search"
             placeholder="enther your need"
           />
-          <button>search</button>
+          <button id={styles.search}>search</button>
             </form>
          
+        </div>
+          <h1>our uniqueness</h1>
+        <div className={styles.unique}>
+          <div className={styles.items}>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error ullam fugit id, est, doloremque voluptate magnam, ab autem reprehenderit porro cupiditate magni ipsa vero voluptates vel. Et iste veritatis in.</p>
+          </div>
+          <div className={styles.items}>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error ullam fugit id, est, doloremque voluptate magnam, ab autem reprehenderit porro cupiditate magni ipsa vero voluptates vel. Et iste veritatis in.</p>
+          </div>
+          <div className={styles.items}>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error ullam fugit id, est, doloremque voluptate magnam, ab autem reprehenderit porro cupiditate magni ipsa vero voluptates vel. Et iste veritatis in.</p>
+          </div>
+          <div className={styles.items}>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error ullam fugit id, est, doloremque voluptate magnam, ab autem reprehenderit porro cupiditate magni ipsa vero voluptates vel. Et iste veritatis in.</p>
+          </div>
         </div>
       </div>
     </div>
   );
 }
 
-export default rentpage;
+export default peershare;
