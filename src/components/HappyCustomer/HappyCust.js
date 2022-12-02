39c183b9fc50b565a79224e9760b499a8c3ccff9
jacobsanosh <jacobsanosh@gmail.com>
2022-12-02 15:22:21 +0530

    adding happy cust

--- src/components/HappyCustomer/HappyCust.js
+++ src/components/HappyCustomer/HappyCust.js
@@ -1,9 +1,77 @@
-import React from 'react'
-import styles from './HappyCust.module.css'
+import React, { useCallback, useState } from "react";
+import customerData from "../../data/customerData";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/swiper.min.css";
+import styles from "./HappyCust.module.css";
+import Image from "next/image";
+import left from "../../assets/icons/angleLeft.png";
+import right from "../../assets/icons/angleRight.png";
+
+
 function HappyCust() {
+  const [custData, setCustData] = useState();
+  // const handleLeftClick = useCallback(() => {
+  //   if (!custData) return custData.slidePrev();
+  // }, [custData]);
+  // const handleRightClick = useCallback(() => {
+  //   if (!custData) return custData.slideNext();
+  // }, [custData]);
   return (
-    <div className='styles'>Happy_cust</div>
-  )
+    <div>
+      <h2>Happy customer</h2>
+      <div className={styles.header}>
+        <div className={styles.heading}>Sanitized & safe cars</div>
+        {/* <div className={styles.arrows}>
+          <Image
+            src={left}
+            alt="left"
+            width={30}
+            height={30}
+            style={{ cursor: "pointer" }}
+            onClick={handleLeftClick}
+          />
+          <Image
+            src={right}
+            alt="right"
+            width={30}
+            height={30}
+            style={{ cursor: "pointer" }}
+            onClick={handleRightClick}
+          />
+        </div> */}
+      </div>
+      <div className={styles.carousel}>
+        <Swiper
+          spaceBetween={30}
+          className="mySwiper"
+          onSwiper={custData}
+          slidesPerView={1}
+          breakpoints={{
+            640: {
+              slidesPerView: 2,
+            },
+            768: {
+              slidesPerView: 3,
+            },
+            1024: {
+              slidesPerView: 4,
+            },
+          }}
+        >
+          {customerData.map((item) => (
+            <SwiperSlide key={item.id}>
+              <div className={styles.main_container}>
+                <div className={styles.section}>
+                  <h2>{item.head}</h2>
+                  <p>{item.para}</p>
+                </div>
+              </div>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </div>
+    </div>
+  );
 }
 
-export default HappyCust
+export default HappyCust;
---
+++ src/data/customerData.js
@@ -0,0 +1,8 @@
+const customerData=[
+    {
+        id:1,
+        head:"Happy customer",
+        para:"Nice service with on time pickup and delivery. Good customer support with friendly and very helpful staff. The car was also very smooth and the packages are pocket friendly. Overall an amazing experience. Would highly recommend😄"
+    }
+]
+export default customerData
