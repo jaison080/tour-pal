b292019af31e107a76d7a2cc33e9d85e39ce5da2
Alen4152 <alenchaks.aj@gmail.com>
2022-12-02 12:41:59 +0530

    nav bar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,9 +1,33 @@
-import React from 'react'
-import styles from './Navbar.module.css'
+import React from "react";
+import styles from "./Navbar.module.css";
 function Navbar() {
   return (
-    <div>Navbar</div>
-  )
+    <div>
+      <nav className={styles.nav}>
+        <div className={styles.container}>
+          <h1 className={styles.logo}>
+            <a href="/">TourPal</a>
+          </h1>
+          <ul>
+            <li>
+              <a href="/" className={styles.current}>
+                Home
+              </a>
+            </li>
+            <li>
+              <a href="/">About</a>
+            </li>
+            <li>
+              <a href="/">Services</a>
+            </li>
+            <li>
+              <a href="/">Contact</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </div>
+  );
 }
 
-export default Navbar
+export default Navbar;
