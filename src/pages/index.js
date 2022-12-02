bd2e61889dc84e354d7a2b86e71b9b94c559ea68
jaison080 <jaisondennis080@gmail.com>
2022-12-02 11:53:41 +0530

    Updated

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,5 @@
 import { Footer, Navbar } from "../components";
 import styles from "../styles/Home.module.css";
-
 export default function Home() {
   return (
     <div className={styles.container}>
